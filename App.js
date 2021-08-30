2c999b004b8274c383ca4d4365426b6d8a032cee
nuhakhangr8 <nuhakhangr8@gmail.com>
2021-08-30 08:05:39 +0530

    Troubleshooting and functions

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 
+var birthdayInput=document.querySelector("#birthday-input");
+var checkButton=document.querySelector("#check-button");
+var outputEl=document.querySelector("#output");
 
 function reverseString(str){
 var str = str.split("");
@@ -29,8 +32,7 @@ function dateToString(date){
         }
     if(date.month<10){
         dateToBeStr.month='0'+date.month;
-    } 
-        else{
+    } else{
             dateToBeStr.month=date.month.toString();
         }
     dateToBeStr.year=date.year.toString();
@@ -54,26 +56,128 @@ function dateFormatVariations(date){
     return dateFormats
 }
 function checksPalindrome(date){
-    var checkDate=dateFormatVariations(date);
+    var allDate=dateFormatVariations(date)
     var check=false;
-    for(var i=0;i<checkDate.length;i++){
-        if(isPalindrome(checkDate[i])){
+    for(var i=0;i<allDate.length;i++){
+        if(isPalindrome(allDate[i])){
             check=true;
             break;
         }
+    }   //maybe I was looping the return check withh for loop
         return check;
+};
+
+
+function leapYear(year){
+    if(year%400===0){
+        return true;
+    }else
+        if(year%100===0){
+            return false;
+        }else{
+            if(year%4===0){
+                return true;
+            }else{
+            return false;
+        }
+    }    
+}
+function upcomingDate(date){
+     var day=date.day+1;
+     var month=date.month;
+     var year=date.year;
+
+     var daysOfMonth=[31,28,31,30,31,30,31,31,30,31,30,31];
+
+    if(month===2)
+       {
+
+         if(leapYear(year))
+         {
+            if(day>29){
+                day=1;
+                month++;
+            }
+        }
+          
+        else{
+           if(day>28)
+            {
+                day=1;
+                month++;
+            }
+        
+        }
+    
+ } else {
+        if(day>daysOfMonth[month-1]){
+            day=1;
+            month++
+        }
     }
-    // var dateFormats=dateFormatVariations(dateFormats);
+    if(month>12){
+        month=1;
+        year++;
+    }
+    return {
+        day:day,
+        month: month,
+        year:year,
+    };
+}
+
+ 
+function getTheNextPalindrome(date){
+    var counter=0;
+    var nextDate=upcomingDate(date);
+    while(1){
+        counter++;
+        var palindrome=checksPalindrome(nextDate);
+        if(palindrome){
+            break;
+        }
+        nextDate=upcomingDate(nextDate);
+    }
+    return [counter,nextDate];
+
+}
+
+//by the time I got to clickhandler 
+//I already forgot how to build it's logic 
+function clickHandler(date){
+    var inputBirthday=birthdayInput.value;
+    
+    if(inputBirthday!==""){
+        var dateList=inputBirthday.split("-")
+        var date={
+            day:Number(dateList[2]),
+            month:Number(dateList[1]),
+            year:Number(dateList[0])
+        };
+       var palindrome=checksPalindrome(date);
+       if(palindrome){
+           outputEl.innerText="Congratulations!!Your birthday date is a shubh palindrome"
+       }
+       else{
+           var[counter,nextDate]=getTheNextPalindrome(date);
+           outputEl.innerText=`The next palindrome date is
+           ${nextDate.day}-${nextDate.month}-${nextDate.year}.Sorry, you missed
+           by ${counter} days`;
+           
+       }
+    }
+
+}
+checkButton.addEventListener("click",clickHandler)
+// var dateFormats=dateFormatVariations(dateFormats);
     // var isPalindrome=isPalindrome(dateFormats)
     // return isPalindrome
-}
-var input={
-    day:"21",
-    month:"1",
-    year:"2021",
-};
-console.log(checksPalindrome(input))
 
+    
+    //   var  Input= { day: 5, month: 1, year: 2020 }
+      
+    //   console.log(getTheNextPalindrome(Input))
+    //I did something wrong the programmes is loading each time I try to call function
 
 
 
